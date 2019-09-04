eaf6a3364a6a35040d8294b60270c79bbc030722
Bogdan Tabusca <btabusca@endava.com>
2019-09-04 10:33:37 +0300

    Add project files.

---
+++ WSChatApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WSChatApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server wsServer = new Server("http://localhost:9000/");
+            wsServer.Start();
+        }
+    }
+}
---
+++ WSChatApp/Server.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.WebSockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WSChatApp
+{
+    public class Server
+    {
+        public Dictionary<int, WebSocket> Connections;
+        public int ClientNumber = 0;
+
+        private readonly HttpListener httpListener;
+        private readonly Mutex signal;
+        public Server(string endpoint)
+        {
+            httpListener = new HttpListener();
+            signal = new Mutex();
+            httpListener.Prefixes.Add(endpoint);
+
+            Connections = new Dictionary<int, WebSocket>();
+        }
+
+        public void Start()
+        {
+            httpListener.Start();
+            while (signal.WaitOne())
+            {
+                Task.Run(async () => await ReceiveConnection().ConfigureAwait(false));
+            }
+        }
+
+        private async Task ReceiveConnection()
+        {
+            var context = await httpListener.GetContextAsync();
+
+            if (context.Request.IsWebSocketRequest)
+            {
+                var webSocketContext = await context.AcceptWebSocketAsync(null);
+                var webSocket = webSocketContext.WebSocket;
+
+                int id;
+                lock (Connections)
+                {
+                    Interlocked.Increment(ref ClientNumber);
+                    id = ClientNumber;
+                    Connections[ClientNumber] = webSocket;
+                }
+
+                await HandleConnection(webSocket, id);
+            }
+
+            signal.ReleaseMutex();
+        }
+
+        private async Task HandleConnection(WebSocket webSocket,int id)
+        {
+            var receivedBytes = new byte[1024];
+
+            await webSocket.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes("Connected.")),
+                WebSocketMessageType.Text, true, CancellationToken.None);
+
+            while (webSocket.State == WebSocketState.Open)
+            {
+                await webSocket.ReceiveAsync(new ArraySegment<byte>(receivedBytes), CancellationToken.None);
+
+                var message = Encoding.Default.GetString(receivedBytes);
+
+                foreach (var connectionId in Connections.Keys)
+                {
+                    await Connections[connectionId].SendAsync(
+                        new ArraySegment<byte>(Encoding.UTF8.GetBytes(id+" "+message),0,12),
+                        WebSocketMessageType.Text, true, CancellationToken.None);
+                }
+
+            }
+        }
+    }
+}
